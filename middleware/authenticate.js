8c8238ae17acf25f6ff6f68bb26f336a7b637db0
mariojuretic <jureticm23@gmail.com>
2018-10-31 08:29:07 +0100

    Setup authentication middleware

---
+++ middleware/authenticate.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+const authenticate = (req, res, next) => {
+  const token = req.header("x-auth");
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return res.status(401).send();
+  }
+
+  User.findOne({ _id: decoded.user_id })
+    .then(user => {
+      if (!user) {
+        return res.status(401).send();
+      }
+
+      req.user = user;
+      next();
+    })
+    .catch(err => res.status(500).send());
+};
+
+module.exports = authenticate;
