79ed930e831057ff0f4fe5ee771db63c94af4ca7
mariojuretic <jureticm23@gmail.com>
2018-10-31 13:18:20 +0100

    Setup basic register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,71 @@
 import React, { Component } from "react";
+import "./Register.css";
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    console.log(newUser);
+  }
+
   render() {
     return (
-      <div>
-        <h1>Sign up</h1>
+      <div className="register">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-6">
+              <h1 className="display-4 text-center">Sign up</h1>
+              <form noValidate onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Email address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  className="btn btn-info btn-block"
+                  value="Create Account"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
